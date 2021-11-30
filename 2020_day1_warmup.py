c6f480e1cc3dc8d20e7c1687bd78a424c44a8d24
Heather Gray <heathergray153@gmail.com>
2021-11-30 14:59:20 -0700

    2020 Day 1 warm up completed

---
+++ 2020_day1_warmup.py
@@ -0,0 +1,61 @@
+# 2020 Day 1 to warm up for the 2021 puzzles coming tomorrow :)
+# Puzzle at: https://adventofcode.com/2020/day/1
+'''
+    Part 1:
+    Before you leave, the Elves in accounting just need you to fix your 
+    expense report (your puzzle input); apparently, something isn't 
+    quite adding up. Specifically, they need you to find the two entries
+    that sum to 2020 and then multiply those two numbers together.
+    
+    Part 2:
+    In your expense report, what is the product of the THREE entries that 
+    sum to 2020?
+'''
+#---------------------------------------------------------------------------
+# AOC framework setup
+from aocd.models import Puzzle
+puzzle = Puzzle(year=2020, day=1)
+#---------------------------------------------------------------------------
+
+# Begin solution #######################
+from itertools import combinations
+
+expense_report = puzzle.input_data.split('\n')
+TARGET_SUM = 2020
+
+def get_product(permutations):
+    
+    combos = [combo for combo in combinations(expense_report, permutations)]
+    product = 1
+    
+    for combo in combos:
+        combo_sum = 0
+        
+        for number in combo:
+            combo_sum += int(number)
+        
+        if combo_sum == TARGET_SUM:
+            for number in combo:
+                product *= int(number)
+    
+    return product
+
+if __name__ == '__main__':
+    answer_day1pt1 = 157059
+    answer_day1pt2 = 165080960
+    
+    test_day1pt1 = 'Fail'
+    test_day1pt2 = 'Fail'
+    
+    pt1_answer = get_product(2)
+    pt2_answer = get_product(3)
+    
+    if pt1_answer == answer_day1pt1:
+        test_day1pt1 = 'Pass'
+    if pt2_answer == answer_day1pt2:
+        test_day1pt2 = 'Pass' 
+    
+    print('T E S T  R E S U L T S')
+    print('-----------------------------------------')
+    print('Day 1 Part 1: ' + test_day1pt1 + ' with answer: ' + str(pt1_answer))
+    print('Day 1 Part 2: ' + test_day1pt2 + ' with answer: ' + str(pt2_answer))
